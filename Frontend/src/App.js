a08d4197866964972502716f9870790bb886922a
User-AjitPatil <ajitpatil7805@gmail.com>
2025-02-05 22:45:23 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,6 +13,7 @@ function App() {
     <Route path='/' element={<Home />} />
     <Route path='/signup' element={<Signup/>} />
     <Route path='/login' element={<Login/>} />
+    <Route path='/login1' element={<Login/>} />
     </Routes>
     </BrowserRouter>
   );
